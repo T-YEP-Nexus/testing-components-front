c5bc1a571f35247d2c74db86e953a479ee232586
Bastian <bastian.cruvellier@epitech.eu>
2025-06-23 17:05:09 +0200

    Init + Hero Landing Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,49 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import { AccessBtn } from "@/components/buttons/AccessBtn";
+import LandingCards from "@/components/cards/LandingCards";
+import { ArrowRight } from "lucide-react";
+import { Russo_One } from "next/font/google";
+
+const russoOne = Russo_One({
+  subsets: ["latin"],
+  weight: "400",
+});
+
+export default function Landing() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="flex flex-col md:flex-row items-center text-white px-6 md:p-10 md:mx-auto md:max-w-6xl">
+      <div className="flex items-center justify-center md:hidden">
+        <img src="/images/Nexus.png" alt="Landing Page" className="max-w-2/3" />
+      </div>
+      <div className="flex flex-col items-center md:items-start max-md:text-center gap-6 flex-2 md:max-w-1/2">
+        <h1 className={`text-3xl font-bold ${russoOne.className}`}>
+          Centralise toutes les plateformes d'apprentissage avec nexus
+        </h1>
+        <p className="text-md">
+          Un seul outil pour tes projets, tes cours, tes rendus et bien plus
+          encore.
+        </p>
+        <AccessBtn
+          size="lg"
+          onClick={() => {
+            alert("Accéder à mon espace");
+          }}
+          className="bg-[#1971FF] py-6 text-white group cursor-pointer md:hover:bg-[#1971FF]/70 transition-all duration-300 shadow-md shadow-black/20"
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+          Accéder à mon espace
+          <ArrowRight
+            className="-me-1 ms-2 transition-transform md:group-hover:translate-x-0.5"
+            size={16}
+            strokeWidth={2}
+            aria-hidden="true"
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        </AccessBtn>
+      </div>
+      <div className="flex flex-1 items-center justify-center max-md:hidden md:justify-end">
+        <img src="/images/Nexus.png" alt="Landing Page" className="max-w-3/4" />
+      </div>
+      <LandingCards />
     </div>
   );
 }
---
+++ components/cards/LandingCards.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function LandingCards() {
+  return (
+    <div>LandingCards</div>
+  )
+}
+
+export default LandingCards
