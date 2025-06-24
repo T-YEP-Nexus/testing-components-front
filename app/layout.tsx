cc139f32993625c58bba068305c285553cf7cd86
Bastian <bastian.cruvellier@epitech.eu>
2025-06-24 11:52:22 +0200

    Bubbles animées

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import BackgroundBubbles from "@/components/Background/BackgroundBubbles";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,8 +26,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased relative`}
       >
+        <BackgroundBubbles />
         {children}
       </body>
     </html>
